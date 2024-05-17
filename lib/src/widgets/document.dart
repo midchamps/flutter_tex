e7f6a9bf7f236a343e3dae935e94ad7fb79e72c6
flexboni <kgb8594@gmail.com>
2024-05-17 14:27:35 +0900

    feat: Add tag type

--- lib/src/widgets/document.dart
+++ lib/src/widgets/document.dart
@@ -2,19 +2,33 @@ import 'package:flutter_tex/flutter_tex.dart';
 import 'package:flutter_tex/src/models/widget_meta.dart';
 import 'package:flutter_tex/src/utils/style_utils.dart';
 
+enum TagType { div, p }
+
 class TeXViewDocument extends TeXViewWidget {
   /// Raw String containing HTML and TEX Code e.g. r"""$$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$<br> """
   final String data;
 
+  final TagType type;
+
+  final Node node;
+
   /// Style TeXView Widget with [TeXViewStyle].
   final TeXViewStyle? style;
 
-  const TeXViewDocument(this.data, {this.style});
+  const TeXViewDocument(
+    this.data, {
+    this.style,
+    this.type = TagType.div,
+    this.node = Node.leaf,
+  });
 
   @override
   TeXViewWidgetMeta meta() {
-    return const TeXViewWidgetMeta(
-        tag: 'div', classList: 'tex-view-document', node: Node.leaf);
+    return TeXViewWidgetMeta(
+      tag: type.name,
+      classList: 'tex-view-document',
+      node: node,
+    );
   }
 
   @override
